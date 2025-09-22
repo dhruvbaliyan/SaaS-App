dfdbb6f9cee8cbd36b9603dd6c269876b427e820
Dhruv-decoders <dhruv.b@decodershq.com>
2025-09-22 11:21:32 +0530

    first commit

---
+++ src/mysite/views.py
@@ -0,0 +1,45 @@
+import pathlib
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from visit.models import PageVisit
+
+def home_view(request,*args, **kwargs):
+    return about_view(request,*args,**kwargs)
+
+def about_view(request,*args, **kwargs):
+    qs= PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    try:
+        percent = page_qs.count()*100/qs.count()
+    except:
+        percent = 0
+    my_title = "My Page"
+    html_template = "home.html"
+    my_context = {
+        "page_title":my_title,
+        "page_visit_count":page_qs.count(),
+        "percent":percent,
+        "total_visit_count":qs.count()
+    }
+    PageVisit.objects.create(path=request.path)
+    return render(request,html_template,my_context)
+
+
+def my_old_home_page_view(request,*args, **kwargs):
+    my_title = "My Page"
+    my_context = {
+        "page-title" : my_title
+    }
+
+    html_= """
+            <!DOCTYPE html>
+            <html>
+
+            <body>
+                <h1>{page_title} anything?</h1>
+            </body>
+            </html>    
+        """.format(**my_context)
+
+    return HttpResponse(html_)
