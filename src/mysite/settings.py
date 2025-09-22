6aa52bf2db647a32f7e0166de68c79b9fac1ac3e
Dhruv-decoders <dhruv.b@decodershq.com>
2025-09-22 15:28:44 +0530

    DB Neon Connected

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from decouple import config,Csv
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # print(BASE_DIR)
@@ -76,10 +77,11 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default':dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age= 600,
+        ssl_require= True
+    )
 }
 
 
